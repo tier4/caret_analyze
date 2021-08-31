87aa8b4877a0acd7a0c15f31bb5b6956ecbfeba2
hsgwa <hasegawa.ats@gmail.com>
2021-08-31 23:00:49 +0900

    clean: divide pybind and impl files
    Signed-off-by: hsgwa <hasegawa.ats@gmail.com>

--- trace_analysis_cpp_impl/src/pybind.cpp
+++ trace_analysis_cpp_impl/src/pybind.cpp
@@ -1,16 +1,15 @@
-#include <pybind11/pybind11.h>
+#include "pybind11/pybind11.h"
+#include "trace_analysis_cpp_impl/record.hpp"
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-int add(int i, int j) {
-    return i + j;
-}
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(cmake_example, m) {
-    m.doc() = R"pbdoc(
+  m.doc() =
+    R"pbdoc(
         Pybind11 example plugin
         -----------------------
 
@@ -23,21 +22,25 @@ PYBIND11_MODULE(cmake_example, m) {
            subtract
     )pbdoc";
 
-    m.def("add", &add, R"pbdoc(
+  m.def(
+    "add", &add,
+    R"pbdoc(
         Add two numbers
 
         Some other explanation about the add function.
     )pbdoc");
 
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+  m.def(
+    "subtract", [](int i,
+    int j) {return i - j;}, R"pbdoc(
         Subtract two numbers
 
         Some other explanation about the subtract function.
     )pbdoc");
 
 #ifdef VERSION_INFO
-    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+  m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
 #else
-    m.attr("__version__") = "dev";
+  m.attr("__version__") = "dev";
 #endif
 }
---
+++ trace_analysis_cpp_impl/src/record.cpp
@@ -0,0 +1,6 @@
+#include "trace_analysis_cpp_impl/record.hpp"
+
+int add(int i, int j)
+{
+  return i + j;
+}
