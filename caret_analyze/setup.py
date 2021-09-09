57965e558c496633c1c44e154f96abbfa755d515
hsgwa <hasegawa.ats@gmail.com>
2021-09-09 19:45:01 +0900

    modify package installation

--- caret_analyze/setup.py
+++ caret_analyze/setup.py
@@ -1,3 +1,4 @@
+from setuptools import find_packages
 from setuptools import setup
 
 package_name = "caret_analyze"
@@ -5,7 +6,7 @@ package_name = "caret_analyze"
 setup(
     name=package_name,
     version="0.0.0",
-    packages=[package_name],
+    packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
