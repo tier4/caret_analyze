2fe952359249fbce866fc544060af35e376ab7bf
hsgwa <hasegawa.ats@gmail.com>
2021-09-16 02:43:30 +0900

    support ros cli
    Signed-off-by: hsgwa <hasegawa.ats@gmail.com>

--- caret_analyze/setup.py
+++ caret_analyze/setup.py
@@ -19,6 +19,5 @@ setup(
     license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [f'caret_create = {package_name}.cli:caret_create'],
     },
 )
