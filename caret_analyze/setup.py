df78ad4a36528421897e06c480317a902c1d9ec0
hsgwa <hasegawa.ats@gmail.com>
2021-09-16 04:52:15 +0900

    v0.1.0
    Signed-off-by: hsgwa <hasegawa.ats@gmail.com>

--- caret_analyze/setup.py
+++ caret_analyze/setup.py
@@ -5,7 +5,7 @@ package_name = 'caret_analyze'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
