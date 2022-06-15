c831d9ff90230fde13404ed7234ab7eef5772f81
atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
2022-06-16 08:41:26 +0900

    docs: add CallbackXXXPlot classes to __all__
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>

--- src/caret_analyze/plot/__init__.py
+++ src/caret_analyze/plot/__init__.py
@@ -14,6 +14,9 @@
 
 from .bokeh.callback_info_factory import Plot
 from .bokeh.callback_info_interface import TimeSeriesPlot
+from .bokeh.callback_info import (CallbackLatencyPlot,
+                                  CallbackJitterPlot,
+                                  CallbackFrequencyPlot)
 from .bokeh.callback_sched import callback_sched
 from .bokeh.message_flow import message_flow
 from .graphviz.callback_graph import callback_graph
@@ -22,6 +25,9 @@ from .graphviz.node_graph import node_graph
 
 __all__ = [
     'Plot',
+    'CallbackLatencyPlot',
+    'CallbackJitterPlot',
+    'CallbackFrequencyPlot',
     'TimeSeriesPlot',
     'callback_graph',
     'callback_sched',
