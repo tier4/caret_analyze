2fbf9c729e082ae867b9a45cceee539767c869f3
atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
2022-06-16 13:43:18 +0900

    docs: pass flake8
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>

--- src/caret_analyze/plot/__init__.py
+++ src/caret_analyze/plot/__init__.py
@@ -12,11 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .bokeh.callback_info import (CallbackFrequencyPlot,
+                                  CallbackJitterPlot,
+                                  CallbackLatencyPlot)
 from .bokeh.callback_info_factory import Plot
 from .bokeh.callback_info_interface import TimeSeriesPlot
-from .bokeh.callback_info import (CallbackLatencyPlot,
-                                  CallbackJitterPlot,
-                                  CallbackFrequencyPlot)
 from .bokeh.callback_sched import callback_sched
 from .bokeh.message_flow import message_flow
 from .graphviz.callback_graph import callback_graph
