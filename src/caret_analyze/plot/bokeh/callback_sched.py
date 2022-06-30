2834cc280a8afe01c1269bdd3f5d78ccac0b4f59
Bo Peng <82485734+bopeng-saitama@users.noreply.github.com>
2022-06-30 12:15:59 +0900

    Revert "fix: fix spell error"
    This reverts commit af052f39aca7a5b870f0fcbf5094017a6d67eb53.

--- src/caret_analyze/plot/bokeh/callback_sched.py
+++ src/caret_analyze/plot/bokeh/callback_sched.py
@@ -110,7 +110,6 @@ def sched_plot_cbg(
                tools=['xwheel_zoom', 'xpan', 'save', 'reset'],
                active_scroll='xwheel_zoom',
                 )
-    p.sizing_mode = 'stretch_width'
     x_range_name = 'x_plot_axis'
     converter: Optional[ClockConverter] = None
     if use_sim_time:
@@ -127,11 +126,11 @@ def sched_plot_cbg(
     rect_y = 0.0
     rect_height = 0.3
     rect_y_step = -1.5
-    callback_name = 0
+    callbcak_num = 0
 
     for callback_group in cbgs:
         for callback in callback_group.callbacks:
-            callback_name += 1
+            callbcak_num += 1
             rect_source = get_callback_rects(callback, clipper, rect_y, rect_height, converter)
             bar_source = get_callback_bar(callback, rect_y, frame_max, frame_min)
             color = color_selector.get_color(
@@ -146,7 +145,7 @@ def sched_plot_cbg(
                    source=rect_source,
                    color=color,
                    alpha=1.0,
-                   legend_label=f'callback{callback_name}',
+                   legend_label=f'callback{callbcak_num}',
                    hover_fill_color=color,
                    hover_alpha=1.0,
                    x_range_name=x_range_name)
@@ -158,7 +157,7 @@ def sched_plot_cbg(
                    'height',
                    source=bar_source,
                    fill_color=color,
-                   legend_label=f'callback{callback_name}',
+                   legend_label=f'callback{callbcak_num}',
                    hover_fill_color=color,
                    hover_alpha=0.1,
                    fill_alpha=0.1,
