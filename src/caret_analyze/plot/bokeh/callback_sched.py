28afdef30d55315cb2deeedecbfeaa9b17edf091
Bo Peng <82485734+bopeng-saitama@users.noreply.github.com>
2022-06-30 14:08:13 +0900

    docs: add comment

--- src/caret_analyze/plot/bokeh/callback_sched.py
+++ src/caret_analyze/plot/bokeh/callback_sched.py
@@ -126,11 +126,11 @@ def sched_plot_cbg(
     rect_y = 0.0
     rect_height = 0.3
     rect_y_step = -1.5
-    callback_name = 0
+    callback_idx = 0
 
     for callback_group in cbgs:
         for callback in callback_group.callbacks:
-            callback_name += 1
+            callback_idx += 1
             rect_source = get_callback_rects(callback, clipper, rect_y, rect_height, converter)
             bar_source = get_callback_bar(callback, rect_y, frame_max, frame_min)
             color = color_selector.get_color(
@@ -145,7 +145,9 @@ def sched_plot_cbg(
                    source=rect_source,
                    color=color,
                    alpha=1.0,
-                   legend_label=f'callback{callback_name}',
+                   legend_label=f'callback{callback_idx}',
+                   # Since setting callback name to legend will narrow the graph,
+                   # sequential numbering is used here.
                    hover_fill_color=color,
                    hover_alpha=1.0,
                    x_range_name=x_range_name)
@@ -157,7 +159,7 @@ def sched_plot_cbg(
                    'height',
                    source=bar_source,
                    fill_color=color,
-                   legend_label=f'callback{callback_name}',
+                   legend_label=f'callback{callback_idx}',
                    hover_fill_color=color,
                    hover_alpha=0.1,
                    fill_alpha=0.1,
