d7ae94d8ce5c37a4217595bb6b9e6afcc053b05c
hsgwa <hasegawa.isp@gmail.com>
2022-07-27 16:11:16 +0900

    correct comment
    Signed-off-by: hsgwa <hasegawa.isp@gmail.com>

--- src/test/record/test_response_time.py
+++ src/test/record/test_response_time.py
@@ -295,14 +295,14 @@ class TestResponseHistogram:
 
         hist, latency = response.to_histogram(1)
         assert list(hist) == [
-            2,  # (1, 2)
-            2,  # (2, 3)
-            1,  # (3, 4)
-            1,  # (4, 5)
-            1,  # (5, 6)
-            1,  # (6, 7)
-            1,  # (7, 8)
-            1   # (8, 9]
+            2,  # [1, 2)
+            2,  # [2, 3)
+            1,  # [3, 4)
+            1,  # [4, 5)
+            1,  # [5, 6)
+            1,  # [6, 7)
+            1,  # [7, 8)
+            1   # [8, 9]
         ]
         assert list(latency) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
