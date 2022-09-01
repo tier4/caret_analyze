tier4/caret_analyze

commit f1c1b5e6a6719a033f513db987584050b5bb61dc

perf(lttng): improve memory consumption (#100)
* remove ros2_tracing impl

Signed-off-by: hsgwa <hasegawa.isp@gmail.com>

* fix: test errors caused by forced_conversion option

Signed-off-by: hsgwa <hasegawa.isp@gmail.com>

* Update src/test/infra/lttng/test_architecture_reader_lttng.py

Signed-off-by: hsgwa <hasegawa.isp@gmail.com>
Co-authored-by: Takayuki AKAMINE <38586589+takam5f2@users.noreply.github.com>

* fix: cannot picke SwigPyObject in yaml dump

Signed-off-by: hsgwa <hasegawa.isp@gmail.com>

* refactor(lttng): wrap bt2 to EventCollection

Signed-off-by: hsgwa <hasegawa.isp@gmail.com>

* feat: support lttng cache

Signed-off-by: hsgwa <hasegawa.isp@gmail.com>

* add: tqdm for converting and loading

Signed-off-by: hsgwa <hasegawa.isp@gmail.com>

* fix: giving events as arguments

Signed-off-by: hsgwa <hasegawa.isp@gmail.com>

* fix: flake8

Signed-off-by: hsgwa <hasegawa.isp@gmail.com>

* add: force_conversion option

Signed-off-by: hsgwa <hasegawa.isp@gmail.com>

Signed-off-by: hsgwa <hasegawa.isp@gmail.com>
Co-authored-by: Takayuki AKAMINE <38586589+takam5f2@users.noreply.github.com>

src/caret_analyze/infra/lttng/lttng.py
@@ -14,22 +14,20 @@
 
 from __future__ import annotations
 
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod, abstractproperty
 from datetime import datetime
 from logging import getLogger
-from typing import Dict, List, Optional, Sequence, Tuple, Union
+import os
+import pickle
+from typing import Any, Dict, Iterable, Iterator, List, Optional, Sequence, Sized, Tuple, Union
 
 import bt2
-
-from caret_analyze.value_objects.timer import TimerValue
-
 import pandas as pd
-
-from tracetools_analysis.loading import load_file
+from tqdm import tqdm
 
 from .events_factory import EventsFactory
-from .ros2_tracing.data_model import DataModel
-from .ros2_tracing.processor import Ros2Handler
+from .ros2_tracing.data_model import Ros2DataModel
+from .ros2_tracing.processor import get_field, Ros2Handler
 from .value_objects import (PublisherValueLttng,
                             SubscriptionCallbackValueLttng,
                             TimerCallbackValueLttng)
@@ -37,7 +35,8 @@ from ..infra_base import InfraBase
 from ...common import ClockConverter
 from ...exceptions import InvalidArgumentError
 from ...record import RecordsInterface
-from ...value_objects import CallbackGroupValue, ExecutorValue, NodeValue, NodeValueWithId, Qos
+from ...value_objects import  \
+    CallbackGroupValue, ExecutorValue, NodeValue, NodeValueWithId, Qos, TimerValue
 
 Event = Dict[str, int]
 
@@ -152,6 +151,130 @@ class EventDurationFilter(LttngEventFilter):
         return self._offset <= elapsed_s and elapsed_s < (self._offset + self._duration)
 
 
+class EventCollection(Iterable, Sized):
+
+    def __init__(self, trace_dir: str, force_conversion: bool) -> None:
+        self._iterable_events: IterableEvents
+        cache_path = self._cache_path(trace_dir)
+
+        if os.path.exists(cache_path) and not force_conversion:
+            logger.info('Found converted file.')
+            self._iterable_events = PickleEventCollection(cache_path)
+        else:
+            self._iterable_events = CtfEventCollection(trace_dir)
+            self._store_cache(self._iterable_events, cache_path)
+            logger.info(f'Converted to {cache_path}')
+
+    @staticmethod
+    def _store_cache(iterable_events: IterableEvents, path) -> None:
+        with open(path, mode='wb') as f:
+            pickle.dump(iterable_events.events, f)
+
+    def __len__(self) -> int:
+        return len(self._iterable_events)
+
+    def __iter__(self) -> Iterator[Dict]:
+        return iter(self._iterable_events)
+
+    def _cache_path(self, events_path: str) -> str:
+        return os.path.join(events_path, 'caret_converted')
+
+
+class IterableEvents(Iterable, Sized, metaclass=ABCMeta):
+
+    @abstractmethod
+    def __iter__(self) -> Iterator[Dict]:
+        pass
+
+    @abstractmethod
+    def __len__(self) -> int:
+        pass
+
+    @abstractproperty
+    def events(self) -> List[Dict]:
+        pass
+
+
+class PickleEventCollection(IterableEvents):
+
+    def __init__(self, events_path: str) -> None:
+        with open(events_path, mode='rb') as f:
+            self._events = pickle.load(f)
+
+    def __iter__(self) -> Iterator[Dict]:
+        return iter(self._events)
+
+    def __len__(self) -> int:
+        return len(self._events)
+
+    @property
+    def events(self) -> List[Dict]:
+        return self._events
+
+
+class CtfEventCollection(IterableEvents):
+
+    def __init__(self, events_path: str) -> None:
+        event_count = 0
+        for msg in bt2.TraceCollectionMessageIterator(events_path):
+            if type(msg) is bt2._EventMessageConst:
+                event_count += 1
+
+            if (not Lttng._last_trace_begin_time
+                    and type(msg) is bt2._PacketBeginningMessageConst):
+                Lttng._last_trace_begin_time = msg.default_clock_snapshot.ns_from_origin
+            elif type(msg) is bt2._PacketEndMessageConst:
+                Lttng._last_trace_end_time = msg.default_clock_snapshot.ns_from_origin
+            # Check for traces lost
+            elif(type(msg) is bt2._DiscardedEventsMessageConst):
+                msg = ('Tracer discarded '
+                       f'{msg.count} events between '
+                       f'{msg.beginning_default_clock_snapshot.ns_from_origin} and '
+                       f'{msg.end_default_clock_snapshot.ns_from_origin}.')
+                logger.warning(msg)
+
+        self._size = event_count
+        self._events = self._to_dicts(events_path, event_count)
+
+    def __iter__(self) -> Iterator[Dict]:
+        return iter(self._events)
+
+    def __len__(self) -> int:
+        return self._size
+
+    @staticmethod
+    def _to_event(msg: Any) -> Dict[str, Any]:
+        event: Dict[str, Any] = {}
+        event[LttngEventFilter.NAME] = msg.event.name
+        event['timestamp'] = msg.default_clock_snapshot.ns_from_origin
+        event.update(msg.event.payload_field)
+        event.update(msg.event.common_context_field)
+        return event
+
+    @property
+    def events(self) -> List[Dict]:
+        return self._events
+
+    @staticmethod
+    def _to_dicts(trace_dir: str, count: int) -> List[Dict]:
+        msg_it = bt2.TraceCollectionMessageIterator(trace_dir)
+        events = []
+        acceptable_tracepoints = set(Ros2Handler.get_trace_points())
+        for msg in tqdm(msg_it, total=count, desc='converting'):
+            if type(msg) is not bt2._EventMessageConst:
+                continue
+            if msg.event.name not in acceptable_tracepoints:
+                continue
+
+            event = CtfEventCollection._to_event(msg)
+            event_dict = {
+                k: get_field(event, k) for k in event
+            }
+            events.append(event_dict)
+
+        return events
+
+
 class Lttng(InfraBase):
     """
     Lttng data container class.
@@ -182,65 +305,80 @@ class Lttng(InfraBase):
         data, events = self._parse_lttng_data(
             trace_dir_or_events,
             force_conversion,
-            event_filters or []
+            event_filters or [],
+            store_events
         )
+        self.data = data
         self._info = LttngInfo(data)
         self._source: RecordsSource = RecordsSource(data, self._info)
         self._counter = EventCounter(data, validate=validate)
-        self.events = events if store_events else None
+        self.events = events
 
     @staticmethod
     def _parse_lttng_data(
         trace_dir_or_events: Union[str, Dict],
         force_conversion: bool,
-        event_filters: List[LttngEventFilter]
-    ) -> Tuple[DataModel, Dict]:
+        event_filters: List[LttngEventFilter],
+        store_events: bool
+    ) -> Tuple[Any, Dict]:
+
+        data = Ros2DataModel()
+        handler = Ros2Handler(data)
+        events = []
+
         if isinstance(trace_dir_or_events, str):
             Lttng._last_trace_begin_time = None
-            for msg in bt2.TraceCollectionMessageIterator(trace_dir_or_events):
-                if (not Lttng._last_trace_begin_time
-                        and type(msg) is bt2._PacketBeginningMessageConst):
-                    Lttng._last_trace_begin_time = msg.default_clock_snapshot.ns_from_origin
-                elif type(msg) is bt2._PacketEndMessageConst:
-                    Lttng._last_trace_end_time = msg.default_clock_snapshot.ns_from_origin
-                # Check for traces lost
-                elif(type(msg) is bt2._DiscardedEventsMessageConst):
-                    msg = ('Tracer discarded '
-                           f'{msg.count} events between '
-                           f'{msg.beginning_default_clock_snapshot.ns_from_origin} and '
-                           f'{msg.end_default_clock_snapshot.ns_from_origin}.')
-                    logger.warning(msg)
+            event_collection = EventCollection(trace_dir_or_events, force_conversion)
+            print('{} events found.'.format(len(event_collection)))
 
+            common = LttngEventFilter.Common()
+            common.start_time = Lttng._last_trace_begin_time
+            common.end_time = Lttng._last_trace_end_time
+
+            filtered_event_count = 0
+            for event in tqdm(
+                    iter(event_collection),
+                    total=len(event_collection),
+                    desc='loading'):
+                if len(event_filters) > 0 and \
+                        any(not f.accept(event, common) for f in event_filters):
+                    continue
+                if store_events:
+                    event_dict = {
+                        k: get_field(event, k) for k in event
+                    }
+                    events.append(event_dict)
+                filtered_event_count += 1
+                event_name = event[LttngEventFilter.NAME]
+                handler_ = handler.handler_map[event_name]
+                handler_(event)
+
+            data.finalize()
             Lttng._last_load_dir = trace_dir_or_events
             Lttng._last_filters = event_filters
-            events = load_file(trace_dir_or_events, force_conversion=force_conversion)
-            print('{} events found.'.format(len(events)))
+            if len(event_filters) > 0:
+                print('filtered to {} events.'.format(filtered_event_count))
         else:
+            # Note: giving events as arguments is used only for debugging.
+            filtered_event_count = 0
             events = trace_dir_or_events
+            for event in events:
+                if len(event_filters) > 0 and \
+                        any(not f.accept(event, common) for f in event_filters):
+                    continue
+                if store_events:
+                    events.append(event)
+                filtered_event_count += 1
+                event_name = event[LttngEventFilter.NAME]
+                handler_ = handler.handler_map[event_name]
+                handler_(event)
+            data.finalize()
+            Lttng._last_filters = event_filters
+            if len(event_filters) > 0:
+                print('filtered to {} events.'.format(filtered_event_count))
 
-        if len(event_filters) > 0:
-            events = Lttng._filter_events(events, event_filters)
-            print('filtered to {} events.'.format(len(events)))
-
-        handler = Ros2Handler.process(events)
-        return handler.data, events
-
-    @staticmethod
-    def _filter_events(events: List[Event], filters: List[LttngEventFilter]):
-        if len(events) == 0:
-            return []
-
-        common = LttngEventFilter.Common()
-        common.start_time = events[0][LttngEventFilter.TIMESTAMP]
-        common.end_time = events[-1][LttngEventFilter.TIMESTAMP]
-
-        filtered = []
-        from tqdm import tqdm
-        for event in tqdm(events):
-            if all(event_filter.accept(event, common) for event_filter in filters):
-                filtered.append(event)
-
-        return filtered
+        events_ = None if len(events) == 0 else events
+        return data, events_
 
     def get_nodes(
         self

commit 91e4947be7040d7337ec31756617b3af6dc7dc76

fix(lttng): error on LttngEventFilter caused by #100
Signed-off-by: hsgwa <hasegawa.isp@gmail.com>

src/caret_analyze/infra/lttng/lttng.py
@@ -47,8 +47,9 @@ class LttngEventFilter(metaclass=ABCMeta):
     NAME = '_name'
     TIMESTAMP = '_timestamp'
     CPU_ID = '_cpuid'
-    VPID = 'vpid'
-    VTID = 'vtid'
+    VPID = '_vpid'
+    VTID = '_vtid'
+    PROCNAME = '_procname'
 
     class Common:
         start_time: int
@@ -267,6 +268,10 @@ class CtfEventCollection(IterableEvents):
                 continue
 
             event = CtfEventCollection._to_event(msg)
+            event[LttngEventFilter.TIMESTAMP] = event.pop('timestamp')
+            event[LttngEventFilter.VTID] = event.pop('vtid')
+            event[LttngEventFilter.VPID] = event.pop('vpid')
+            event[LttngEventFilter.PROCNAME] = event.pop('procname')
             event_dict = {
                 k: get_field(event, k) for k in event
             }

src/caret_analyze/infra/lttng/ros2_tracing/processor.py
@@ -180,8 +180,8 @@ class Ros2Handler():
         event: Dict,
     ) -> None:
         context_handle = get_field(event, 'context_handle')
-        timestamp = get_field(event, 'timestamp')
-        pid = get_field(event, 'vpid')
+        timestamp = get_field(event, '_timestamp')
+        pid = get_field(event, '_vpid')
         version = get_field(event, 'version')
         self.data.add_context(pid, context_handle, timestamp, version)
 
@@ -190,8 +190,8 @@ class Ros2Handler():
         event: Dict,
     ) -> None:
         handle = get_field(event, 'node_handle')
-        timestamp = get_field(event, 'timestamp')
-        tid = get_field(event, 'vtid')
+        timestamp = get_field(event, '_timestamp')
+        tid = get_field(event, '_vtid')
         rmw_handle = get_field(event, 'rmw_handle')
         name = get_field(event, 'node_name')
         namespace = get_field(event, 'namespace')
@@ -202,7 +202,7 @@ class Ros2Handler():
         event: Dict,
     ) -> None:
         handle = get_field(event, 'publisher_handle')
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         node_handle = get_field(event, 'node_handle')
         rmw_handle = get_field(event, 'rmw_publisher_handle')
         topic_name = get_field(event, 'topic_name')
@@ -215,7 +215,7 @@ class Ros2Handler():
         event: Dict,
     ) -> None:
         handle = get_field(event, 'subscription_handle')
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         node_handle = get_field(event, 'node_handle')
         rmw_handle = get_field(event, 'rmw_subscription_handle')
         topic_name = get_field(event, 'topic_name')
@@ -234,7 +234,7 @@ class Ros2Handler():
         event: Dict,
     ) -> None:
         subscription_pointer = get_field(event, 'subscription')
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         handle = get_field(event, 'subscription_handle')
         self.data.add_rclcpp_subscription(
             subscription_pointer, timestamp, handle)
@@ -244,7 +244,7 @@ class Ros2Handler():
         event: Dict,
     ) -> None:
         subscription_pointer = get_field(event, 'subscription')
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         callback_object = get_field(event, 'callback')
         self.data.add_callback_object(
             subscription_pointer, timestamp, callback_object)
@@ -254,7 +254,7 @@ class Ros2Handler():
         event: Dict,
     ) -> None:
         handle = get_field(event, 'service_handle')
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         node_handle = get_field(event, 'node_handle')
         rmw_handle = get_field(event, 'rmw_service_handle')
         service_name = get_field(event, 'service_name')
@@ -266,7 +266,7 @@ class Ros2Handler():
         event: Dict,
     ) -> None:
         handle = get_field(event, 'service_handle')
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         callback_object = get_field(event, 'callback')
         self.data.add_callback_object(handle, timestamp, callback_object)
 
@@ -275,7 +275,7 @@ class Ros2Handler():
         event: Dict,
     ) -> None:
         handle = get_field(event, 'client_handle')
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         node_handle = get_field(event, 'node_handle')
         rmw_handle = get_field(event, 'rmw_client_handle')
         service_name = get_field(event, 'service_name')
@@ -287,9 +287,9 @@ class Ros2Handler():
         event: Dict,
     ) -> None:
         handle = get_field(event, 'timer_handle')
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         period = get_field(event, 'period')
-        tid = get_field(event, 'vtid')
+        tid = get_field(event, '_vtid')
         self.data.add_timer(tid, handle, timestamp, period)
 
     def _handle_rclcpp_timer_callback_added(
@@ -297,7 +297,7 @@ class Ros2Handler():
         event: Dict,
     ) -> None:
         handle = get_field(event, 'timer_handle')
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         callback_object = get_field(event, 'callback')
         self.data.add_callback_object(handle, timestamp, callback_object)
 
@@ -306,7 +306,7 @@ class Ros2Handler():
         event: Dict,
     ) -> None:
         handle = get_field(event, 'timer_handle')
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         node_handle = get_field(event, 'node_handle')
         self.data.add_timer_node_link(handle, timestamp, node_handle)
 
@@ -315,7 +315,7 @@ class Ros2Handler():
         event: Dict,
     ) -> None:
         callback_object = get_field(event, 'callback')
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         symbol = get_field(event, 'symbol')
         self.data.add_callback_symbol(callback_object, timestamp, symbol)
 
@@ -325,7 +325,7 @@ class Ros2Handler():
     ) -> None:
         # Add to dict
         callback = get_field(event, 'callback')
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         is_intra_process = get_field(event, 'is_intra_process')
         self.data.add_callback_start_instance(
             timestamp, callback, is_intra_process)
@@ -336,7 +336,7 @@ class Ros2Handler():
     ) -> None:
         # Fetch from dict
         callback = get_field(event, 'callback')
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         self.data.add_callback_end_instance(timestamp, callback)
 
     def _handle_rcl_lifecycle_state_machine_init(
@@ -351,7 +351,7 @@ class Ros2Handler():
         self,
         event: Dict,
     ) -> None:
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         state_machine = get_field(event, 'state_machine')
         start_label = get_field(event, 'start_label')
         goal_label = get_field(event, 'goal_label')
@@ -363,9 +363,9 @@ class Ros2Handler():
         event: Dict,
     ) -> None:
         publisher_handle = get_field(event, 'publisher_handle')
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         message = get_field(event, 'message')
-        tid = get_field(event, 'vtid')
+        tid = get_field(event, '_vtid')
         message_timestamp = get_field(event, 'message_timestamp')
         self.data.add_rclcpp_publish_instance(
             tid, timestamp, publisher_handle, message, message_timestamp)
@@ -375,8 +375,8 @@ class Ros2Handler():
         event: Dict,
     ) -> None:
         publisher_handle = get_field(event, 'publisher_handle')
-        timestamp = get_field(event, 'timestamp')
-        tid = get_field(event, 'vtid')
+        timestamp = get_field(event, '_timestamp')
+        tid = get_field(event, '_vtid')
         message = get_field(event, 'message')
         self.data.add_rcl_publish_instance(
             tid, timestamp, publisher_handle, message)
@@ -387,7 +387,7 @@ class Ros2Handler():
     ) -> None:
         original_message = get_field(event, 'original_message')
         constructed_message = get_field(event, 'constructed_message')
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         self.data.add_message_construct_instance(
             timestamp, original_message, constructed_message)
 
@@ -397,9 +397,9 @@ class Ros2Handler():
     ) -> None:
         message = get_field(event, 'message')
         publisher_handle = get_field(event, 'publisher_handle')
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         message_timestamp = get_field(event, 'message_timestamp')
-        tid = get_field(event, 'vtid')
+        tid = get_field(event, '_vtid')
         self.data.add_rclcpp_intra_publish_instance(
             tid, timestamp, publisher_handle, message, message_timestamp)
 
@@ -409,7 +409,7 @@ class Ros2Handler():
     ) -> None:
         callback_object = get_field(event, 'callback')
         message = get_field(event, 'message')
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         source_stamp = get_field(event, 'source_stamp')
         message_timestamp = get_field(event, 'message_timestamp')
         self.data.add_dispatch_subscription_callback_instance(
@@ -422,7 +422,7 @@ class Ros2Handler():
     ) -> None:
         callback_object = get_field(event, 'callback')
         message = get_field(event, 'message')
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         message_timestamp = get_field(event, 'message_timestamp')
         self.data.add_dispatch_intra_process_subscription_callback_instance(
             timestamp, callback_object, message, message_timestamp
@@ -432,7 +432,7 @@ class Ros2Handler():
         self,
         event: Dict,
     ) -> None:
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         source_stamp = get_field(event, 'source_stamp')
         self.data.add_on_data_available_instance(timestamp, source_stamp)
 
@@ -440,18 +440,18 @@ class Ros2Handler():
         self,
         event: Dict,
     ) -> None:
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         message = get_field(event, 'message')
-        tid = get_field(event, 'vtid')
+        tid = get_field(event, '_vtid')
         self.data.add_dds_write_instance(tid, timestamp, message)
 
     def _handle_dds_bind_addr_to_stamp(
         self,
         event: Dict,
     ) -> None:
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         addr = get_field(event, 'addr')
-        tid = get_field(event, 'vtid')
+        tid = get_field(event, '_vtid')
         source_stamp = get_field(event, 'source_stamp')
         self.data.add_dds_bind_addr_to_stamp(tid, timestamp, addr, source_stamp)
 
@@ -459,7 +459,7 @@ class Ros2Handler():
         self,
         event: Dict,
     ) -> None:
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         addr_from = get_field(event, 'addr_from')
         addr_to = get_field(event, 'addr_to')
         self.data.add_dds_bind_addr_to_addr(timestamp, addr_from, addr_to)
@@ -475,7 +475,7 @@ class Ros2Handler():
         self,
         event: Dict,
     ) -> None:
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         executor_addr = get_field(event, 'executor_addr')
         executor_type_name = get_field(event, 'executor_type_name')
         self.data.add_executor(executor_addr, timestamp, executor_type_name)
@@ -484,7 +484,7 @@ class Ros2Handler():
         self,
         event: Dict,
     ) -> None:
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         executor_addr = get_field(event, 'executor_addr')
         collector_addr = get_field(event, 'entities_collector_addr')
         executor_type_name = get_field(event, 'executor_type_name')
@@ -494,7 +494,7 @@ class Ros2Handler():
         self,
         event: Dict,
     ) -> None:
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         executor_addr = get_field(event, 'executor_addr')
         callback_group_addr = get_field(event, 'callback_group_addr')
         group_type_name = get_field(event, 'group_type_name')
@@ -505,7 +505,7 @@ class Ros2Handler():
         self,
         event: Dict,
     ) -> None:
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         collector_addr = get_field(event, 'entities_collector_addr')
         callback_group_addr = get_field(event, 'callback_group_addr')
         group_type_name = get_field(event, 'group_type_name')
@@ -516,7 +516,7 @@ class Ros2Handler():
         self,
         event: Dict,
     ) -> None:
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         callback_group_addr = get_field(event, 'callback_group_addr')
         timer_handle = get_field(event, 'timer_handle')
         self.data.callback_group_add_timer(callback_group_addr, timestamp, timer_handle)
@@ -525,7 +525,7 @@ class Ros2Handler():
         self,
         event: Dict,
     ) -> None:
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         callback_group_addr = get_field(event, 'callback_group_addr')
         subscription_handle = get_field(event, 'subscription_handle')
         self.data.callback_group_add_subscription(
@@ -535,7 +535,7 @@ class Ros2Handler():
         self,
         event: Dict,
     ) -> None:
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         callback_group_addr = get_field(event, 'callback_group_addr')
         service_handle = get_field(event, 'service_handle')
         self.data.callback_group_add_service(callback_group_addr, timestamp, service_handle)
@@ -544,7 +544,7 @@ class Ros2Handler():
         self,
         event: Dict,
     ) -> None:
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         callback_group_addr = get_field(event, 'callback_group_addr')
         client_handle = get_field(event, 'client_handle')
         self.data.callback_group_add_client(callback_group_addr, timestamp, client_handle)
@@ -553,7 +553,7 @@ class Ros2Handler():
         self,
         event: Dict,
     ) -> None:
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         subscription = get_field(event, 'subscription')
         node_name = get_field(event, 'node_name')
         topic_name = get_field(event, 'topic_name')
@@ -563,7 +563,7 @@ class Ros2Handler():
         self,
         event: Dict,
     ) -> None:
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         publisher = get_field(event, 'publisher')
         node_name = get_field(event, 'node_name')
         topic_name = get_field(event, 'topic_name')
@@ -573,7 +573,7 @@ class Ros2Handler():
         self,
         event: Dict,
     ) -> None:
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         subscription = get_field(event, 'subscription')
         tilde_message_id = get_field(event, 'tilde_message_id')
         self.data.add_tilde_subscribe(timestamp, subscription, tilde_message_id)
@@ -596,7 +596,7 @@ class Ros2Handler():
         self,
         event: Dict,
     ) -> None:
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         subscription_id = get_field(event, 'subscription_id')
         node_name = get_field(event, 'node_name')
         topic_name = get_field(event, 'topic_name')
@@ -606,6 +606,6 @@ class Ros2Handler():
         self,
         event: Dict,
     ) -> None:
-        timestamp = get_field(event, 'timestamp')
+        timestamp = get_field(event, '_timestamp')
         sim_time = get_field(event, 'stamp')
         self.data.add_sim_time(timestamp, sim_time)