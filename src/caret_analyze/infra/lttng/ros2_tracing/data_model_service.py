6ce4fddfda0bf164f7bc3e14a5c7cc870191b00a
atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
2022-09-23 08:55:22 +0900

    feat(ros2_tracing): add API to get node_name from cbg_addr
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>

--- src/caret_analyze/infra/lttng/ros2_tracing/data_model_service.py
+++ src/caret_analyze/infra/lttng/ros2_tracing/data_model_service.py
@@ -39,10 +39,100 @@ class DataModelService:
         self,
         cbg_addr: int
     ) -> Optional[str]:
-        raise NotImplementedError
+        node_name = self._get_node_name_from_cbg_timer(cbg_addr)
+        if node_name:
+            return node_name
+
+        node_name = self._get_node_name_from_cbg_sub(cbg_addr)
+        if node_name:
+            return node_name
+
+        return None
+
+    def _get_node_name_from_cbg_timer(self, cbg_addr: int) -> Optional[str]:
+        print('a')
+        match_cbg_timer = self._data.callback_group_timer.query(
+            f'callback_group_addr == {cbg_addr}'
+        )
+        if match_cbg_timer.empty:
+            return None
+
+        assert len(match_cbg_timer) == 1
+        timer_handle = match_cbg_timer.iloc[0]['timer_handle']
+        match_timer_node_links = self._data.timer_node_links.query(
+            f'timer_handle == {timer_handle}'
+        )
+        if match_timer_node_links.empty:
+            return None
+
+        assert len(match_timer_node_links) == 1
+        node_handle = match_timer_node_links.iloc[0]['node_handle']
+        match_nodes = self._data.nodes.query(
+            f'node_handle == {node_handle}'
+        )
+        if match_nodes.empty:
+            return None
+
+        assert len(match_nodes) == 1
+        node_name = (match_nodes.iloc[0]['namespace']
+                     + '/' + match_nodes.iloc[0]['name'])
+        return node_name
+
+    def _get_node_name_from_cbg_sub(self, cbg_addr: int) -> Optional[str]:
+        match_cbg_sub = self._data.callback_group_subscription.query(
+            f'callback_group_addr == {cbg_addr}'
+        )
+        if match_cbg_sub.empty:
+            return None
+
+        assert len(match_cbg_sub) == 1
+        subscription_handle = match_cbg_sub.iloc[0]['subscription_handle']
+        match_subscriptions = self._data.subscriptions.query(
+            f'subscription_handle == {subscription_handle}'
+        )
+        if match_subscriptions.empty:
+            return None
+
+        assert len(match_subscriptions) == 1
+        node_handle = match_subscriptions.iloc[0]['node_handle']
+        match_nodes = self._data.nodes.query(
+            f'node_handle == {node_handle}'
+        )
+        if match_nodes.empty:
+            return None
+
+        assert len(match_nodes) == 1
+        node_name = (match_nodes.iloc[0]['namespace']
+                     + '/' + match_nodes.iloc[0]['name'])
+        return node_name
 
     def _get_node_name_from_get_parameters_srv(
         self,
         cbg_addr: int
     ) -> Optional[str]:
-        raise NotImplementedError
+        match_cbg_srv = self._data.callback_group_service.query(
+            f'callback_group_addr == {cbg_addr}'
+        )
+        if match_cbg_srv.empty:
+            return None
+
+        assert len(match_cbg_srv) == 1
+        service_handle = match_cbg_srv.iloc[0]['service_handle']
+        match_services = self._data.services.query(
+            f'service_handle == {service_handle}'
+        )
+        if match_services.empty:
+            return None
+
+        assert len(match_services) == 1
+        node_handle = match_services.iloc[0]['node_handle']
+        match_nodes = self._data.nodes.query(
+            f'node_handle == {node_handle}'
+        )
+        if match_nodes.empty:
+            return None
+
+        assert len(match_nodes) == 1
+        node_name = (match_nodes.iloc[0]['namespace']
+                     + '/' + match_nodes.iloc[0]['name'])
+        return node_name
