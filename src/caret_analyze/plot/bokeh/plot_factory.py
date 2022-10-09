9b820d5580109a1e5fe144c165a92110f8832511
atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
2022-10-09 14:48:15 +0900

    refactor(plot): allow unpack
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>

--- src/caret_analyze/plot/bokeh/plot_factory.py
+++ src/caret_analyze/plot/bokeh/plot_factory.py
@@ -15,6 +15,8 @@
 from logging import getLogger
 from typing import Collection, Union
 
+from multimethod import multimethod as singledispatchmethod
+
 from .callback_info import (CallbackFrequencyPlot,
                             CallbackLatencyPlot,
                             CallbackPeriodPlot)
@@ -25,6 +27,7 @@ from .communication_info import (CommunicationFrequencyPlot,
 from .communication_info_interface import CommunicationTimeSeriesPlot
 from .pub_sub_info import PubSubFrequencyPlot, PubSubPeriodPlot
 from .pub_sub_info_interface import PubSubTimeSeriesPlot
+from ...exceptions import InvalidArgumentError
 from ...runtime import (CallbackBase, Communication, Publisher, Subscription)
 
 logger = getLogger(__name__)
@@ -32,26 +35,77 @@ logger = getLogger(__name__)
 
 class Plot:
 
-    @staticmethod
-    def create_callback_frequency_plot(
-        callbacks: Collection[CallbackBase]
-    ) -> TimeSeriesPlot:
+    @singledispatchmethod
+    def create_callback_frequency_plot(arg) -> TimeSeriesPlot:
         """
         Get CallbackFrequencyPlot instance.
 
         Parameters
         ----------
         callbacks : Collection[CallbackBase]
+            Target callbacks.
+            This also accepts multiple CallbackBase inputs by unpacking.
 
         Returns
         -------
         CallbackFrequencyPlot
 
         """
+        raise InvalidArgumentError(f'Unknown argument type: {arg}')
+
+    @staticmethod
+    @create_callback_frequency_plot.register
+    def _create_callback_frequency_plot(
+        callbacks: Collection[CallbackBase]
+    ) -> TimeSeriesPlot:
         return CallbackFrequencyPlot(callbacks)
 
     @staticmethod
-    def create_callback_jitter_plot(
+    @create_callback_frequency_plot.register
+    def _create_callback_frequency_plot_tuple(
+        *callbacks: CallbackBase
+    ) -> TimeSeriesPlot:
+        return CallbackFrequencyPlot(callbacks)
+
+    @singledispatchmethod
+    def create_callback_period_plot(arg) -> TimeSeriesPlot:
+        """
+        Get CallbackPeriodPlot instance.
+
+        Parameters
+        ----------
+        callbacks : Collection[CallbackBase]
+            Target callbacks.
+            This also accepts multiple CallbackBase inputs by unpacking.
+
+        Returns
+        -------
+        CallbackPeriodPlot
+
+        """
+        raise InvalidArgumentError(f'Unknown argument type: {arg}')
+
+    @staticmethod
+    @create_callback_period_plot.register
+    def _create_callback_period_plot(
+        callbacks: Collection[CallbackBase]
+    ) -> TimeSeriesPlot:
+        return CallbackPeriodPlot(callbacks)
+
+    @staticmethod
+    @create_callback_period_plot.register
+    def _create_callback_period_plot_tuple(
+        *callbacks: CallbackBase
+    ) -> TimeSeriesPlot:
+        return CallbackPeriodPlot(callbacks)
+
+    @singledispatchmethod
+    def create_callback_jitter_plot(arg) -> TimeSeriesPlot:
+        raise InvalidArgumentError(f'Unknown argument type: {arg}')
+
+    @staticmethod
+    @create_callback_jitter_plot.register
+    def _create_callback_jitter_plot(
         callbacks: Collection[CallbackBase]
     ) -> TimeSeriesPlot:
         logger.warning('create_callback_jitter_plot is deprecated.'
@@ -59,67 +113,132 @@ class Plot:
         return Plot.create_callback_period_plot(callbacks)
 
     @staticmethod
-    def create_callback_period_plot(
-        callbacks: Collection[CallbackBase]
+    @create_callback_jitter_plot.register
+    def _create_callback_jitter_plot_tuple(
+        *callbacks: CallbackBase
     ) -> TimeSeriesPlot:
-        """
-        Get CallbackPeriodPlot instance.
+        logger.warning('create_callback_jitter_plot is deprecated.'
+                       'Use create_callback_period_plot')
+        return Plot.create_callback_period_plot(callbacks)
 
-        Parameters
-        ----------
-        callbacks: Collection[CallbackBase]
-
-        Returns
-        -------
-        CallbackPeriodPlot
-
-        """
-        return CallbackPeriodPlot(callbacks)
-
-    @staticmethod
-    def create_callback_latency_plot(
-        callbacks: Collection[CallbackBase]
-    ) -> TimeSeriesPlot:
+    @singledispatchmethod
+    def create_callback_latency_plot(arg) -> TimeSeriesPlot:
         """
         Get CallbackLatencyPlot instance.
 
         Parameters
         ----------
-        callbacks: Collection[CallbackBase]
+        callbacks : Collection[CallbackBase]
+            Target callbacks.
+            This also accepts multiple CallbackBase inputs by unpacking.
 
         Returns
         -------
         CallbackLatencyPlot
 
         """
+        raise InvalidArgumentError(f'Unknown argument type: {arg}')
+
+    @staticmethod
+    @create_callback_latency_plot.register
+    def _create_callback_latency_plot(
+        callbacks: Collection[CallbackBase]
+    ) -> TimeSeriesPlot:
         return CallbackLatencyPlot(callbacks)
 
     @staticmethod
-    def create_publish_subscription_period_plot(
+    @create_callback_latency_plot.register
+    def _create_callback_latency_plot_tuple(
+        *callbacks: CallbackBase
+    ) -> TimeSeriesPlot:
+        return CallbackLatencyPlot(callbacks)
+
+    @singledispatchmethod
+    def create_publish_subscription_period_plot(arg) -> TimeSeriesPlot:
+        raise InvalidArgumentError(f'Unknown argument type: {arg}')
+
+    @staticmethod
+    @create_publish_subscription_period_plot.register
+    def _create_publish_subscription_period_plot(
         pub_subs: Collection[Union[Publisher, Subscription]]
     ) -> PubSubTimeSeriesPlot:
         return PubSubPeriodPlot(pub_subs)
 
     @staticmethod
-    def create_publish_subscription_frequency_plot(
+    @create_publish_subscription_period_plot.register
+    def _create_publish_subscription_period_plot_tuple(
+        *pub_subs: Union[Publisher, Subscription]
+    ) -> PubSubTimeSeriesPlot:
+        return PubSubPeriodPlot(pub_subs)
+
+    @singledispatchmethod
+    def create_publish_subscription_frequency_plot(arg) -> TimeSeriesPlot:
+        raise InvalidArgumentError(f'Unknown argument type: {arg}')
+
+    @staticmethod
+    @create_publish_subscription_frequency_plot.register
+    def _create_publish_subscription_frequency_plot(
         pub_subs: Collection[Union[Publisher, Subscription]]
     ) -> PubSubTimeSeriesPlot:
         return PubSubFrequencyPlot(pub_subs)
 
     @staticmethod
-    def create_communication_latency_plot(
+    @create_publish_subscription_frequency_plot.register
+    def _create_publish_subscription_frequency_plot_tuple(
+        *pub_subs: Union[Publisher, Subscription]
+    ) -> PubSubTimeSeriesPlot:
+        return PubSubFrequencyPlot(pub_subs)
+
+    @singledispatchmethod
+    def create_communication_latency_plot(arg) -> TimeSeriesPlot:
+        raise InvalidArgumentError(f'Unknown argument type: {arg}')
+
+    @staticmethod
+    @create_communication_latency_plot.register
+    def _create_communication_latency_plot(
         communications: Collection[Communication]
     ) -> CommunicationTimeSeriesPlot:
         return CommunicationLatencyPlot(communications)
 
     @staticmethod
-    def create_communication_frequency_plot(
+    @create_communication_latency_plot.register
+    def _create_communication_latency_plot_tuple(
+        *communications: Communication
+    ) -> CommunicationTimeSeriesPlot:
+        return CommunicationLatencyPlot(communications)
+
+    @singledispatchmethod
+    def create_communication_frequency_plot(arg) -> TimeSeriesPlot:
+        raise InvalidArgumentError(f'Unknown argument type: {arg}')
+
+    @staticmethod
+    @create_communication_frequency_plot.register
+    def _create_communication_frequency_plot(
         communications: Collection[Communication]
     ) -> CommunicationTimeSeriesPlot:
         return CommunicationFrequencyPlot(communications)
 
     @staticmethod
-    def create_communication_period_plot(
+    @create_communication_frequency_plot.register
+    def _create_communication_frequency_plot_tuple(
+        *communications: Communication
+    ) -> CommunicationTimeSeriesPlot:
+        return CommunicationFrequencyPlot(communications)
+
+    @singledispatchmethod
+    def create_communication_period_plot(arg) -> TimeSeriesPlot:
+        raise InvalidArgumentError(f'Unknown argument type: {arg}')
+
+    @staticmethod
+    @create_communication_period_plot.register
+    def _create_communication_period_plot(
         communications: Collection[Communication]
+    ) -> CommunicationTimeSeriesPlot:
+        return CommunicationPeriodPlot(communications)
+
+    @staticmethod
+    @create_communication_period_plot.register
+    def _create_communication_period_plot_tuple(
+        *communications: Communication
     ) -> CommunicationTimeSeriesPlot:
         return CommunicationPeriodPlot(communications)
