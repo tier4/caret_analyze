ad4bfedd633d74af80940331966d00aa5fd2d5d8
atsushi yano <55824710+atsushi421@users.noreply.github.com>
2022-10-25 12:03:21 +0900

    feat(record): add API to get frequency time-series data (#116)
    * test: add tests for latency, period, and frequency
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * test(record): fix frequency test case and remove latency and period tests
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * test(record) fix argument passing
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * test(record): fix initial_timestamp to base_timestamp and improve readability
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * test(record): fix columns
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * refactor: add Frequency class into record
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * style(plot): improve readability
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * fix(plot): fix logic error
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * docs(record): add docstring in frequency
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * perf(record): change not to use records.get_column_series
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * style(record): rename function name
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * docs(record): fix docstring
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * tests(record): add one_interval_contains_all_timestamps_case
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * fix(record): address IndexError
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * fix(record): address KeyError in record.get()
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * fix(record): support for cases where zero frequency occurs
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * style(record): improve readability of frequency.py
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * fix(record/frequency): support for cases where base_timestamp is greater than minimum timestamp
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * style(record): improve readability of frequency.py
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * style(record/frequency): improve readability
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * fix(record/frequency): fix how to check whether timestamp exists or not
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    * fix(record): fix test to confirm whether specified column was used
    
    Co-authored-by: hsgwa <19860128+hsgwa@users.noreply.github.com>
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    Co-authored-by: hsgwa <19860128+hsgwa@users.noreply.github.com>

---
+++ src/test/record/test_frequency.py
@@ -0,0 +1,229 @@
+# Copyright 2021 Research Institute of Systems Planning, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from caret_analyze.record import ColumnValue
+from caret_analyze.record.frequency import Frequency
+from caret_analyze.record.record_factory import RecordFactory, RecordsFactory
+
+
+def create_records(records_raw, columns):
+    records = RecordsFactory.create_instance()
+    for column in columns:
+        records.append_column(column, [])
+
+    for record_raw in records_raw:
+        record = RecordFactory.create_instance(record_raw)
+        records.append(record)
+    return records
+
+
+def to_dict(records):
+    return [record.data for record in records]
+
+
+class TestFrequencyRecords:
+
+    def test_empty_case(self):
+        records_raw = [
+        ]
+        columns = [ColumnValue('timestamp')]
+        records = create_records(records_raw, columns)
+
+        frequency = Frequency(records)
+
+        expect_raw = [
+        ]
+        result = to_dict(frequency.to_records())
+        assert result == expect_raw
+
+    def test_one_column_default_case(self):
+        records_raw = [
+            {'timestamp': 0},
+            {'timestamp': 1},
+            {'timestamp': 11},
+            {'timestamp': 12},
+            {'timestamp': 14},
+        ]
+        columns = [ColumnValue('timestamp')]
+        records = create_records(records_raw, columns)
+
+        frequency = Frequency(records)
+
+        expect_raw = [
+            {'timestamp': 0, 'frequency': 2},
+            {'timestamp': 10, 'frequency': 3}
+        ]
+        result = to_dict(frequency.to_records(interval_ns=10))
+        assert result == expect_raw
+
+    def test_specify_interval_ns_case(self):
+        records_raw = [
+            {'timestamp': 1000000000},
+            {'timestamp': 1000500000},
+            {'timestamp': 2000000000},
+            {'timestamp': 2000300000},
+            {'timestamp': 3000000000},
+        ]
+        columns = [ColumnValue('timestamp')]
+        records = create_records(records_raw, columns)
+
+        frequency = Frequency(records)
+
+        expect_raw = [
+            {'timestamp': 1000000000, 'frequency': 4},
+            {'timestamp': 3000000000, 'frequency': 1}
+        ]
+        result = to_dict(frequency.to_records(interval_ns=2000000000))
+        assert result == expect_raw
+
+    def test_specify_base_timestamp_case(self):
+        records_raw = [
+            {'timestamp': 5},
+            {'timestamp': 6},
+            {'timestamp': 11},
+            {'timestamp': 12},
+            {'timestamp': 15},
+        ]
+        columns = [ColumnValue('timestamp')]
+        records = create_records(records_raw, columns)
+
+        frequency = Frequency(records)
+
+        expect_raw = [
+            {'timestamp': 4, 'frequency': 4},
+            {'timestamp': 14, 'frequency': 1}
+        ]
+        result = to_dict(frequency.to_records(interval_ns=10,
+                                              base_timestamp=4))
+        assert result == expect_raw
+
+    def test_two_column_default_case(self):
+        records_raw = [
+            {'timestamp1': 0, 'timestamp2': 2},
+            {'timestamp1': 3, 'timestamp2': 4},
+            {'timestamp1': 11, 'timestamp2': 12},
+            {'timestamp1': 13, 'timestamp2': 14},
+            {'timestamp1': 15, 'timestamp2': 16},
+        ]
+        columns = [ColumnValue('timestamp1'), ColumnValue('timestamp2')]
+        records = create_records(records_raw, columns)
+
+        frequency = Frequency(records)
+
+        expect_raw = [
+            {'timestamp1': 0, 'frequency': 2},
+            {'timestamp1': 10, 'frequency': 3}
+        ]
+        result = to_dict(frequency.to_records(interval_ns=10))
+        assert result == expect_raw
+
+    def test_specify_target_column_case(self):
+        records_raw = [
+            {'timestamp1': 0, 'timestamp2': 2},
+            {'timestamp1': 3, 'timestamp2': 4},
+            {'timestamp1': 5, 'timestamp2': 12},
+            {'timestamp1': 13, 'timestamp2': 14},
+            {'timestamp1': 15, 'timestamp2': 16},
+        ]
+        columns = [ColumnValue('timestamp1'), ColumnValue('timestamp2')]
+        records = create_records(records_raw, columns)
+
+        frequency = Frequency(records, target_column='timestamp2')
+
+        expect_raw = [
+            {'timestamp2': 2, 'frequency': 2},
+            {'timestamp2': 12, 'frequency': 3}
+        ]
+        result = to_dict(frequency.to_records(interval_ns=10))
+        assert result == expect_raw
+
+    def test_drop_case(self):
+        records_raw = [
+            {'timestamp1': 0, 'timestamp2': 2},
+            {'timestamp2': 4},
+            {'timestamp1': 11, 'timestamp2': 12},
+            {'timestamp1': 13, 'timestamp2': 14},
+            {'timestamp1': 15, 'timestamp2': 16},
+        ]
+        columns = [ColumnValue('timestamp1'), ColumnValue('timestamp2')]
+        records = create_records(records_raw, columns)
+
+        frequency = Frequency(records)
+
+        expect_raw = [
+            {'timestamp1': 0, 'frequency': 1},
+            {'timestamp1': 10, 'frequency': 3}
+        ]
+        result = to_dict(frequency.to_records(interval_ns=10))
+        assert result == expect_raw
+
+    def test_one_interval_contains_all_timestamps_case(self):
+        records_raw = [
+            {'timestamp': 0},
+            {'timestamp': 1},
+            {'timestamp': 2},
+            {'timestamp': 3},
+            {'timestamp': 4},
+        ]
+        columns = [ColumnValue('timestamp')]
+        records = create_records(records_raw, columns)
+
+        frequency = Frequency(records)
+
+        expect_raw = [
+            {'timestamp': 0, 'frequency': 5}
+        ]
+        result = to_dict(frequency.to_records(interval_ns=10))
+        assert result == expect_raw
+
+    def test_exist_zero_frequency_case(self):
+        records_raw = [
+            {'timestamp': 0},
+            {'timestamp': 1},
+            {'timestamp': 21},
+            {'timestamp': 22},
+            {'timestamp': 24},
+        ]
+        columns = [ColumnValue('timestamp')]
+        records = create_records(records_raw, columns)
+
+        frequency = Frequency(records)
+
+        expect_raw = [
+            {'timestamp': 0, 'frequency': 2},
+            {'timestamp': 10, 'frequency': 0},
+            {'timestamp': 20, 'frequency': 3}
+        ]
+        result = to_dict(frequency.to_records(interval_ns=10))
+        assert result == expect_raw
+
+    def test_base_ts_greater_than_min_ts_case(self):
+        records_raw = [
+            {'timestamp': 0},
+            {'timestamp': 1},
+            {'timestamp': 11},
+            {'timestamp': 12},
+            {'timestamp': 24},
+        ]
+        columns = [ColumnValue('timestamp')]
+        records = create_records(records_raw, columns)
+
+        frequency = Frequency(records)
+
+        expect_raw = [
+            {'timestamp': 10, 'frequency': 2},
+            {'timestamp': 20, 'frequency': 1}
+        ]
+        result = to_dict(frequency.to_records(interval_ns=10, base_timestamp=10))
+        assert result == expect_raw
