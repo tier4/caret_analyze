73bd2b631e9a8bd4e1fba03ac567b72dae759406
bopeng-saitama <peng.b.203@ms.saitama-u.ac.jp>
2022-10-26 11:26:19 +0900

    fix: output change to one plot
    Signed-off-by: bopeng-saitama <peng.b.203@ms.saitama-u.ac.jp>

--- src/caret_analyze/plot/bokeh/histogram.py
+++ src/caret_analyze/plot/bokeh/histogram.py
@@ -6,6 +6,8 @@ from caret_analyze.record import ResponseTime
 
 from .histogram_interface import HistPlot
 
+from .plot_util import PlotColorSelector
+
 
 class ResponseTimePlot(HistPlot):
 
@@ -23,12 +25,15 @@ class ResponseTimePlot(HistPlot):
         self._case = case
 
     def _show_core(self):
-        for i, path in enumerate(self._target, 1):
-            try:
-                records = path.to_records()
-                response = ResponseTime(records)
-            except IndexError:
-                pass
+        p = figure(plot_width=600,
+                   plot_height=400,
+                   active_scroll='wheel_zoom',
+                   x_axis_label='Response Time [ms]',
+                   y_axis_label='Probability')
+        color_selector = PlotColorSelector()
+        for _, path in enumerate(self._target):
+            records = path.to_records()
+            response = ResponseTime(records)
 
             if self._case == 'default':
                 hist, bins = response.to_histogram(binsize_ns=10000000)
@@ -37,15 +42,10 @@ class ResponseTimePlot(HistPlot):
             elif self._case == 'worst':
                 hist, bins = response.to_worst_case_histogram(binsize_ns=10000000)
 
-            p = figure(plot_width=600,
-                       plot_height=400,
-                       title=f'{path.path_name}',
-                       active_scroll='wheel_zoom',
-                       x_axis_label='Response Time [ms] (period: 10ms)',
-                       y_axis_label='Probability')
             hist = hist / sum(hist)
 
             bins = bins*10**-6
+            color = color_selector.get_color(path.path_name)
             p.quad(top=hist, bottom=0, left=bins[:-1], right=bins[1:],
-                   line_color='white', alpha=0.5)
-            show(p)
+                   color=color, alpha=0.5, legend_label=f'{path.path_name}')
+        show(p)
