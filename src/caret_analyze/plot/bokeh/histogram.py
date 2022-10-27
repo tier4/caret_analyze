5a181aa0bd67c7e77a7ca834ccad167105eeb5eb
bopeng-saitama <peng.b.203@ms.saitama-u.ac.jp>
2022-10-27 17:52:06 +0900

    fix: add Copyright
    Signed-off-by: bopeng-saitama <peng.b.203@ms.saitama-u.ac.jp>

--- src/caret_analyze/plot/bokeh/histogram.py
+++ src/caret_analyze/plot/bokeh/histogram.py
@@ -1,3 +1,17 @@
+# Copyright 2021 Research Institute of Systems Planning, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from bokeh.plotting import figure, show
 
 from caret_analyze.exceptions import UnsupportedTypeError
@@ -49,3 +63,4 @@ class ResponseTimePlot(HistPlot):
             p.quad(top=hist, bottom=0, left=bins[:-1], right=bins[1:],
                    color=color, alpha=0.5, legend_label=f'{path.path_name}')
         show(p)
+        return p
