1e85575fcde6e71fba9ea526e30d31f4f80d1a51
hsgwa <19860128+hsgwa@users.noreply.github.com>
2022-11-16 10:20:04 +0900

    pass flake8
    Signed-off-by: hsgwa <19860128+hsgwa@users.noreply.github.com>

--- src/caret_analyze/plot/bokeh/plot_util.py
+++ src/caret_analyze/plot/bokeh/plot_util.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Dict, List, Sequence, Tuple
+from typing import Dict, Sequence
 
 from bokeh.colors import Color, RGB
 
