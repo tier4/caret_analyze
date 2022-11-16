369c916867c0794d672efe7792f56e211474cb7c
atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
2022-11-16 10:16:53 +0900

    style(common): improve error message in type_check_decorator
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>

--- src/caret_analyze/common/type_check_decorator.py
+++ src/caret_analyze/common/type_check_decorator.py
@@ -12,9 +12,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import List
+
+from ..exceptions import InvalidArgumentError
+
+
 try:
-    from pydantic import validate_arguments
-    type_check_decorator = validate_arguments(config={'arbitrary_types_allowed': True})
+    from pydantic import validate_arguments, ValidationError
+
+    def decorator(func):
+        validate_arguments_wrapper = \
+            validate_arguments(config={'arbitrary_types_allowed': True})(func)
+
+        def _custom_wrapper(*args, **kargs):
+            try:
+                validate_arguments_wrapper(*args, **kargs)
+            except ValidationError as e:
+                expected_types: List[str] = []
+                for error in e.errors():
+                    if error['type'] == 'type_error.arbitrary_type':
+                        expected_types.append(error['ctx']['expected_arbitrary_type'])
+                    else:
+                        expected_types.append(error['type'].replace('type_error.', ''))
+                if len(expected_types) == 1:
+                    expected_types_str = f"'{expected_types[0]}'"
+                else:
+                    expected_types_str = str(expected_types)
+
+                if len(error['loc']) == 2:  # (argument_name, index)
+                    loc_str = f'`{error["loc"][0]}`[{error["loc"][1]}]'
+                else:
+                    loc_str = f'`{error["loc"][0]}`'
+
+                msg = f'Type of argument {loc_str} must be {expected_types_str}\n'
+                raise InvalidArgumentError(msg) from None
+        return _custom_wrapper
+    type_check_decorator = decorator
+
 except ImportError:
     def empty_decorator(func):
         def _wrapper(*args, **kargs):
