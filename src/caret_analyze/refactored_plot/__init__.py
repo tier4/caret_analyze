fd1bbff770db58943291d5118de69375d0028f5e
atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
2022-12-07 18:53:59 +0900

    chore: remove MetricsBase from __init__
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>

--- src/caret_analyze/refactored_plot/__init__.py
+++ src/caret_analyze/refactored_plot/__init__.py
@@ -12,12 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .metrics_base import MetricsBase
 from .plot_base import PlotBase
 from .plot_facade import Plot
 
 __all__ = [
     'Plot',
-    'PlotBase',
-    'MetricsBase'
+    'PlotBase'
 ]
