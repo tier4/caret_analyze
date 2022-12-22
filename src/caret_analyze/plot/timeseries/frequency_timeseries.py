e34f1a055ea89bc10818bb8de503b236d7862486
atsushi yano <55824710+atsushi421@users.noreply.github.com>
2022-12-22 21:25:14 +0900

    display empty figure if there is no data (#223)
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
    
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>

--- src/caret_analyze/plot/timeseries/frequency_timeseries.py
+++ src/caret_analyze/plot/timeseries/frequency_timeseries.py
@@ -122,8 +122,7 @@ class FrequencyTimeSeries(MetricsBase):
             if isinstance(last_timestamp, int):
                 last_timestamps.append(last_timestamp)
 
-        # TODO: Even if target_objects.to_records is empty, we want to display an empty graph.
-        # if len(first_timestamps) == 0 or len(last_timestamps) == 0:
-        #     return 0, 1
-        # else:
-        return min(first_timestamps), max(last_timestamps)
+        if len(first_timestamps) == 0 or len(last_timestamps) == 0:
+            return 0, 1  # Intended to show an empty figure.
+        else:
+            return min(first_timestamps), max(last_timestamps)
