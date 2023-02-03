5253dcbf96b82e4bfe67ff22c8e84bfa9de30c05
rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>
2023-02-03 12:43:38 +0900

    fix: typo
    Signed-off-by: rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>

--- src/caret_analyze/architecture/architecture.py
+++ src/caret_analyze/architecture/architecture.py
@@ -378,7 +378,7 @@ class Architecture(Summarizable):
 
     def rename_callback(self, src: str, dst: str) -> None:
         """
-        Update callback name "src" to "dst" in arhchitecture.
+        Update callback name "src" to "dst" in architecture.
 
         Parameters
         ----------
@@ -396,7 +396,7 @@ class Architecture(Summarizable):
 
     def rename_node(self, src: str, dst: str) -> None:
         """
-        Update node name "src" to "dst" in arhchitecture.
+        Update node name "src" to "dst" in architecture.
 
         Parameters
         ----------
@@ -417,7 +417,7 @@ class Architecture(Summarizable):
 
     def rename_path(self, src: str, dst: str) -> None:
         """
-        Update path name "src" to "dst" in arhchitecture.
+        Update path name "src" to "dst" in architecture.
 
         Parameters
         ----------
@@ -432,7 +432,7 @@ class Architecture(Summarizable):
 
     def rename_executor(self, src: str, dst: str) -> None:
         """
-        Update executor name "src" to "dst" in arhchitecture.
+        Update executor name "src" to "dst" in architecture.
 
         Parameters
         ----------
@@ -447,7 +447,7 @@ class Architecture(Summarizable):
 
     def rename_topic(self, src: str, dst: str) -> None:
         """
-        Update topic name "src" to "dst" in arhchitecture.
+        Update topic name "src" to "dst" in architecture.
 
         Parameters
         ----------
