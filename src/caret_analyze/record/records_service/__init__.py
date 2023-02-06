a4a33c03c1631fadebcd3d78e1a009a758e034b2
atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
2023-02-06 17:03:45 +0900

    feat(plot): add Range class in record
    Signed-off-by: atsushi421 <a.yano.578@ms.saitama-u.ac.jp>

--- src/caret_analyze/record/records_service/__init__.py
+++ src/caret_analyze/record/records_service/__init__.py
@@ -15,11 +15,13 @@
 from .frequency import Frequency
 from .latency import Latency
 from .period import Period
-from . response_time import ResponseTime
+from .range import Range
+from .response_time import ResponseTime
 
 __all__ = [
     'Frequency',
     'Latency',
     'Period',
+    'Range',
     'ResponseTime',
 ]
---
+++ src/caret_analyze/record/records_service/range.py
@@ -0,0 +1,52 @@
+# Copyright 2021 Research Institute of Systems Planning, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from logging import getLogger
+from typing import List, Optional, Sequence, Tuple
+
+from ..interface import RecordsInterface
+
+
+logger = getLogger(__name__)
+
+
+class Range:
+
+    def __init__(self, records_list: Sequence[RecordsInterface]) -> None:
+        self._records_list = records_list
+
+    def get_range(self) -> Tuple[int, int]:
+        def remove_none(series: Sequence[Optional[int]]) -> List[int]:
+            return [v for v in series if v is not None]
+
+        # NOTE:
+        # The first column is system time for now.
+        # The other columns could be other than system time.
+        # Only the system time is picked out here.
+        base_series = [remove_none(r.get_column_series(r.columns[0])) for r in self._records_list]
+        min_series: List[int] = []
+        max_series: List[int] = []
+        for series in base_series:
+            if len(series) > 0:
+                min_series.append(min(series))
+                max_series.append(max(series))
+
+        has_valid_data = len(min_series) > 0 or len(max_series) > 0
+        if has_valid_data:
+            range_min = min(min_series)
+            range_max = max(max_series)
+            return range_min, range_max
+        else:
+            logger.warning('No valid measurement data.')
+            return 0, 1
