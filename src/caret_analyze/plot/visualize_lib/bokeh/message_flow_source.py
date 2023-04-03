4aa1a3ea52ae62f7910cfef69aa8ae19326b6a9b
yamasaki <114902604+ymski@users.noreply.github.com>
2023-04-03 16:15:51 +0900

    fixed branching conditions.
    Signed-off-by: yamasaki <114902604+ymski@users.noreply.github.com>

--- src/caret_analyze/plot/visualize_lib/bokeh/message_flow_source.py
+++ src/caret_analyze/plot/visualize_lib/bokeh/message_flow_source.py
@@ -293,8 +293,8 @@ class NodeLevelFormatter(DataFrameFormatter):
             if parsed_before.node_name is not None and parsed.node_name is not None and \
                     parsed_before.node_name == parsed.node_name and \
                     'rclcpp_publish' not in parsed.tracepoint_name and \
-                    not ('callback_start' not in parsed_before.tracepoint_name and
-                         'callback_end' not in parsed.tracepoint_name):
+                    not ('callback_start' in parsed_before.tracepoint_name and
+                         'callback_end' in parsed.tracepoint_name):
                 drop_columns.append(column_name)
 
             if parsed_before.topic_name is not None and parsed.topic_name is not None and \
