fdc0e0ab30786c132a10db29866a708d4e72906c
atsushi421 <yff81986@nifty.com>
2023-05-01 10:14:55 +0900

    feat(record): add row_filter for Frequency class
    Signed-off-by: atsushi421 <yff81986@nifty.com>

--- src/test/record/records_service/test_frequency.py
+++ src/test/record/records_service/test_frequency.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from caret_analyze.record import ColumnValue, Frequency
+from caret_analyze.record import ColumnValue, Frequency, RecordInterface
 from caret_analyze.record.record_factory import RecordsFactory
 
 
@@ -241,4 +241,53 @@ class TestFrequencyRecords:
             {'timestamp': 20, 'frequency': 1}
         ]
         result = to_dict(frequency.to_records(interval_ns=10, base_timestamp=10))
+        assert result == expect_raw
+
+    def test_two_column_drop_non_target_column_case(self):
+        records_raw = [
+            {'timestamp1': 0, 'timestamp2': 2},
+            {'timestamp1': 3},
+            {'timestamp1': 11, 'timestamp2': 12},
+            {'timestamp1': 13, 'timestamp2': 14},
+            {'timestamp1': 15, 'timestamp2': 16},
+        ]
+        columns = [ColumnValue('timestamp1'), ColumnValue('timestamp2')]
+        records = create_records(records_raw, columns)
+
+        frequency = Frequency(records, target_column='timestamp1')
+
+        expect_raw = [
+            {'timestamp1': 0, 'frequency': 2},
+            {'timestamp1': 10, 'frequency': 3}
+        ]
+        result = to_dict(frequency.to_records(interval_ns=10))
+        assert result == expect_raw
+
+    def test_two_column_apply_row_filter_case(self):
+        records_raw = [
+            {'timestamp1': 0, 'timestamp2': 2},
+            {'timestamp1': 3},
+            {'timestamp1': 11, 'timestamp2': 12},
+            {'timestamp1': 13, 'timestamp2': 14},
+            {'timestamp1': 15, 'timestamp2': 16},
+        ]
+        columns = [ColumnValue('timestamp1'), ColumnValue('timestamp2')]
+        records = create_records(records_raw, columns)
+
+        def row_filter(record: RecordInterface) -> bool:
+            start_column = 'timestamp1'
+            end_column = 'timestamp2'
+            if (record.data.get(start_column) is not None
+                    and record.data.get(end_column) is not None):
+                return True
+            else:
+                return False
+
+        frequency = Frequency(records, target_column='timestamp1', row_filter=row_filter)
+
+        expect_raw = [
+            {'timestamp1': 0, 'frequency': 1},
+            {'timestamp1': 10, 'frequency': 3}
+        ]
+        result = to_dict(frequency.to_records(interval_ns=10))
         assert result == expect_raw
