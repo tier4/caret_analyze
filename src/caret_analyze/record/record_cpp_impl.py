0a740a6458d5ea095120edb529c2b426b67b61b0
h-suzuki <h-suzuki@isp.co.jp>
2023-11-17 18:57:24 +0900

    Revert "fix: revert"
    This reverts commit bafb604ed8f1877797364f517f857d216f170808.
    
    Signed-off-by: h-suzuki <h-suzuki@isp.co.jp>

--- src/caret_analyze/record/record_cpp_impl.py
+++ src/caret_analyze/record/record_cpp_impl.py
@@ -28,6 +28,7 @@ class RecordCppImpl(RecordBase, RecordInterface):
 
 
 class RecordsCppImpl(RecordsInterface):
+
     def __init__(
         self,
         init: Sequence[RecordInterface] | None = None,
@@ -46,37 +47,47 @@ class RecordsCppImpl(RecordsInterface):
         data_dict = [dic.data for dic in self.data]
         s = yaml.dump(data_dict)
 
-        with open(path, mode="w") as f:
+        with open(path, mode='w') as f:
             f.write(s)
         return None
 
-    def _append_dict(self, other: dict[str, int]) -> None:
+    def _append_dict(
+        self,
+        other: dict[str, int]
+    ) -> None:
         record = RecordBase(other)
         self._records.append(record)
 
-    def _append_record(self, other: RecordInterface) -> None:
+    def _append_record(
+        self,
+        other: RecordInterface
+    ) -> None:
         unknown_columns = set(other.columns) - set(self.columns)
         if len(unknown_columns) > 0:
-            msg = "Contains an unknown columns. "
-            msg += f"{unknown_columns}"
+            msg = 'Contains an unknown columns. '
+            msg += f'{unknown_columns}'
             raise InvalidArgumentError(msg)
 
         self._records.append(other)
 
-    def concat(self, other: RecordsInterface) -> None:
+    def concat(
+        self, other: RecordsInterface
+    ) -> None:
         assert isinstance(other, RecordsCppImpl)
         unknown_columns = set(other.columns) - set(self.columns)
         if len(unknown_columns) > 0:
-            msg = "Contains an unknown columns. "
-            msg += f"{unknown_columns}"
+            msg = 'Contains an unknown columns. '
+            msg += f'{unknown_columns}'
             raise InvalidArgumentError(msg)
         self._records.concat(other._records)
         return None
 
-    def sort(self, key: str, sub_key: str | None = None, ascending=True) -> None:
+    def sort(
+        self, key: str, sub_key: str | None = None, ascending=True
+    ) -> None:
         if key not in self.columns:
-            raise InvalidArgumentError(f"column [{key}] not found.")
-        self._records.sort(key, sub_key or "", ascending)
+            raise InvalidArgumentError(f'column [{key}] not found.')
+        self._records.sort(key, sub_key or '', ascending)
         return None
 
     def sort_column_order(
@@ -93,7 +104,9 @@ class RecordsCppImpl(RecordsInterface):
         data_dict = [record.data for record in self.data]
         return Records._to_dataframe(data_dict, self.columns)
 
-    def rename_columns(self, columns: dict[str, str]) -> None:
+    def rename_columns(
+        self, columns: dict[str, str]
+    ) -> None:
         validate_rename_rule(columns)
         self._records.rename_columns(columns)
         self._columns.rename(columns)
@@ -107,21 +120,24 @@ class RecordsCppImpl(RecordsInterface):
         columns: list[str],
         how: str,
     ) -> RecordsCppImpl:
-        assert how in ["inner", "left", "right", "outer"]
+        assert how in ['inner', 'left', 'right', 'outer']
         assert isinstance(right_records, RecordsCppImpl)
 
         Records._validate_merge_records(columns, self, right_records)
 
         merged_cpp_base = self._records.merge(
-            right_records._records, join_left_key, join_right_key, columns, how
-        )
+            right_records._records, join_left_key, join_right_key,
+            columns, how)
 
         column_values = [ColumnValue(c) for c in columns]
         merged = RecordsCppImpl(None, column_values)
         merged._insert_records(merged_cpp_base)
         return merged
 
-    def groupby(self, columns: list[str]) -> dict[tuple[int, ...], RecordsInterface]:
+    def groupby(
+        self,
+        columns: list[str]
+    ) -> dict[tuple[int, ...], RecordsInterface]:
         assert 0 < len(columns) and len(columns) <= 3
 
         group_cpp_base = self._records.groupby(*columns)
@@ -135,7 +151,7 @@ class RecordsCppImpl(RecordsInterface):
 
     def get_row_series(self, index: int) -> RecordInterface:
         if index >= len(self.data):
-            raise InvalidArgumentError("index exceeds the row size.")
+            raise InvalidArgumentError('index exceeds the row size.')
         return self.data[index]
 
     def get_column_series(self, column_name: str) -> Sequence[int | None]:
@@ -155,8 +171,8 @@ class RecordsCppImpl(RecordsInterface):
     def reindex(self, columns: list[str]) -> None:
         miss_match_columns = set(columns) ^ set(self.columns)
         if len(miss_match_columns) > 0:
-            msg = "Contains an unknown columns. "
-            msg += f"{miss_match_columns}"
+            msg = 'Contains an unknown columns. '
+            msg += f'{miss_match_columns}'
             raise InvalidArgumentError(msg)
         self._records.reindex(columns)
         self._columns.reindex(columns)
@@ -170,16 +186,20 @@ class RecordsCppImpl(RecordsInterface):
     def _insert_records(self, records: RecordsBase) -> None:
         self._records = records
 
-    def append_column(self, column: ColumnValue, values: list[int]) -> None:
+    def append_column(
+        self,
+        column: ColumnValue,
+        values: list[int]
+    ) -> None:
         if len(values) != len(self):
-            raise InvalidArgumentError("len(values) != len(records)")
+            raise InvalidArgumentError('len(values) != len(records)')
 
         self._columns.append(Column(column))
         self._records.append_column(column.column_name, values)
 
     def drop_columns(self, column_names: list[str]) -> None:
         if not isinstance(column_names, list):
-            raise InvalidArgumentError("columns must be list.")
+            raise InvalidArgumentError('columns must be list.')
         self._columns.drop(column_names)
         self._records.drop_columns(column_names)
 
@@ -227,8 +247,8 @@ class RecordsCppImpl(RecordsInterface):
             right_records._records,
             left_stamp_key,
             right_stamp_key,
-            join_left_key or "",
-            join_right_key or "",
+            join_left_key or '',
+            join_right_key or '',
             columns,
             how,
         )
@@ -274,19 +294,12 @@ class RecordsCppImpl(RecordsInterface):
         return merged
 
     def check_null(self) -> bool:
-        """
-        Conpare records.coulmns with record.columns.
-        'record[0]' is representive value under the assumprion that record has same columns.
-        """
         record = self.data[0]
-        return len(record.columns) == len(self.columns)
-
+        if(len(record.columns) == len(self.columns)):
+            return True
+        else:
+            return False
 
     def get_null_columns(self) -> list[str]:
-        """
-        Conpare records.coulmns with record.columns.
-        'record[0]' is representive value under the assumprion that record has same columns.
-        """
-        record = self.data[0]
-        null_columns = set(self.columns) - record.columns
+        null_columns = set(self.columns) - self.data[0].columns
         return list(null_columns)
