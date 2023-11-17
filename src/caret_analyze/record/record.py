723ffef480d00eddf9ffec7977e51baaee901753
h-suzuki <h-suzuki@isp.co.jp>
2023-11-17 20:04:47 +0900

    fix: 1 blank line required between summary line and description
    Signed-off-by: h-suzuki <h-suzuki@isp.co.jp>
    
    fix: 1 blank line between summary line and description
    
    Signed-off-by: h-suzuki <h-suzuki@isp.co.jp>

--- src/caret_analyze/record/record.py
+++ src/caret_analyze/record/record.py
@@ -793,17 +793,19 @@ class Records(RecordsInterface):
 
         return group
 
-
     def check_null(self) -> bool:
-        """Compare records.columns with record.columns.
+        """
+        Compare records.columns with record.columns.
+
         'record[0]' is sample value under the assumption that record has same columns.
         """
         record = self.data[0]
         return len(record.columns) == len(self.columns)
 
-
     def get_null_columns(self) -> list[str]:
-        """Compare records.columns with record.columns.
+        """
+        Compare records.columns with record.columns.
+
         'record[0]' is sample value under the assumption that record has same columns.
         """
         record = self.data[0]
