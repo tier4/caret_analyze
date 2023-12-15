c4309165520038f5ec5c382083217d30232ad57a
emb4 <yuki.emori@tier4.jp>
2023-12-15 11:21:43 +0900

    Fixed doc_string in ExecutorValue.
    Signed-off-by: emb4 <yuki.emori@tier4.jp>

--- src/caret_analyze/value_objects/executor.py
+++ src/caret_analyze/value_objects/executor.py
@@ -103,7 +103,7 @@ class ExecutorStructValue(ValueObject, Summarizable):
 
 
 class ExecutorValue(ValueObject):
-    """Executor info for architecture."""
+    """Executor vlue class."""
 
     def __init__(
         self,
@@ -112,18 +112,58 @@ class ExecutorValue(ValueObject):
         *,
         executor_name: str | None = None
     ) -> None:
+        """
+        Return executor type name.
+
+        Parameters
+        ----------
+        executor_type_name : str
+            Executor type name.
+        callback_group_ids : tuple[str, ...]
+            Callback group ids.
+        executor_name: str | None
+            Executor name.
+
+        """
         self._executor_type = ExecutorType(executor_type_name)
         self._cbg_ids = callback_group_ids
         self._executor_name = executor_name
 
     @property
     def executor_type(self) -> ExecutorType:
+        """
+        Return executor type.
+
+        Returns
+        -------
+        ExecutorType
+            Executor type.
+
+        """
         return self._executor_type
 
     @property
     def callback_group_ids(self) -> tuple[str, ...]:
+        """
+        Return callback group ids.
+
+        Returns
+        -------
+        tuple[str, ...]
+            Callback group id list.
+
+        """
         return self._cbg_ids
 
     @property
     def executor_name(self) -> str | None:
+        """
+        Return executor name.
+
+        Returns
+        -------
+        str
+            Executor name.
+
+        """
         return self._executor_name
