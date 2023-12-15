8fb80c28d21788a0971895eb3a23947636692fa2
emb4 <yuki.emori@tier4.jp>
2023-12-15 17:07:23 +0900

    Fixed doc_string in InheritUniqueStamp.
    Signed-off-by: emb4 <yuki.emori@tier4.jp>

--- src/caret_analyze/value_objects/message_context.py
+++ src/caret_analyze/value_objects/message_context.py
@@ -209,9 +209,26 @@ class InheritUniqueStamp(MessageContext):
 
     @property
     def context_type(self) -> MessageContextType:
+        """
+        Get context type.
+
+        Returns
+        -------
+        MessageContextType
+            Message context type.
+
+        """
         return MessageContextType.INHERIT_UNIQUE_STAMP
 
     def verify(self) -> bool:
+        """
+        Get verify.
+
+        Returns
+        -------
+        bool
+
+        """
         return False
 
 
