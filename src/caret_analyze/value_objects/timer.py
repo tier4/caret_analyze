32d4a5d7250c4fbde94eaab09dd31848ef68bc73
rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>
2023-12-18 14:26:45 +0900

    docs: fix doc string TimerValue
    Signed-off-by: rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>

--- src/caret_analyze/value_objects/timer.py
+++ src/caret_analyze/value_objects/timer.py
@@ -30,6 +30,25 @@ class TimerValue(ValueObject):
         callback_id: str | None,
         construction_order: int
     ) -> None:
+        """
+        Construct an instance.
+
+        Parameters
+        ----------
+        period : int
+            Period of the timer.
+        node_name : str
+            Node name.
+        node_id : str | None
+            Node unique id,
+            a value that can be identified when retrieved from the Architecture reader.
+        callback_id : str | None
+            Callback unique id,
+            a value that can be identified when retrieved from the Architecture reader.
+        construction_order : int
+            Order of instance creation within the identical node.
+
+        """
         self._node_name = node_name
         self._node_id = node_id
         self._period = period
@@ -38,22 +57,67 @@ class TimerValue(ValueObject):
 
     @property
     def construction_order(self) -> int:
+        """
+        Get construction order.
+
+        Returns
+        -------
+        int
+            Construction order.
+
+        """
         return self._construction_order
 
     @property
     def node_name(self) -> str:
+        """
+        Get node name.
+
+        Returns
+        -------
+        str
+            Node name.
+
+        """
         return self._node_name
 
     @property
     def node_id(self) -> str | None:
+        """
+        Get node id.
+
+        Returns
+        -------
+        str | None
+            Node id.
+
+        """
         return self._node_id
 
     @property
     def period(self) -> int:
+        """
+        Get period.
+
+        Returns
+        -------
+        int
+            Period of the timer.
+
+        """
         return self._period
 
     @property
     def callback_id(self) -> str | None:
+        """
+        Get callback id.
+
+        Returns
+        -------
+        str | None
+            Callback unique id.
+
+        """
         return self._callback_id
 
 
