397fb5a5ac8f4e916d2efbb143385d71fb246623
Nobuhiko Uetsuki <uetsuki@isp.co.jp>
2023-12-27 18:22:51 +0900

    Revert "feat: support for no light arguremnt with topic_tools RelayNode in visualization (#432)"
    This reverts commit d81cd6705354f04a03706b7f04f7faf28e244b93.

--- src/caret_analyze/infra/lttng/records_provider_lttng.py
+++ src/caret_analyze/infra/lttng/records_provider_lttng.py
@@ -1613,7 +1613,7 @@ class FilteredRecordsSource:
                     ColumnValue(COLUMN_NAME.SOURCE_TIMESTAMP),
                 ]
             )
-        sample_records = grouped_records[publisher_handles[0]]
+        sample_records = list(grouped_records.values())[0]
         column_values = Columns.from_str(sample_records.columns).to_value()
         pub_records = RecordsFactory.create_instance(None, columns=column_values)
 
@@ -1792,21 +1792,6 @@ class FilteredRecordsSource:
     def _grouped_publish_records(self) -> dict[int, RecordsInterface]:
         records = self._lttng.compose_publish_records()
         group = records.groupby([COLUMN_NAME.PUBLISHER_HANDLE])
-
-        # Compare records.columns with record.columns, and drop mismatched columns.
-        # When node has GenericPublisher, some trace event are not output.
-        for records_key in group:
-            # sample_record_columns is sample value because record.data[N] has same columns.
-            sample_records_columns = set(group[records_key].columns)
-            sample_record_columns: set = group[records_key].data[0].columns
-            mismatched_columns = sample_records_columns - sample_record_columns
-            if mismatched_columns:
-                optional_columns = {
-                    COLUMN_NAME.RCL_PUBLISH_TIMESTAMP,
-                    COLUMN_NAME.DDS_WRITE_TIMESTAMP
-                    }
-                drop_columns = list(optional_columns & mismatched_columns)
-                group[records_key].drop_columns(drop_columns)
         return self._expand_key_tuple(group)
 
     @cached_property
