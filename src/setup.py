d373a843d4ff4f015e36ced37097eb08a1c9130d
h-suzuki <h-suzuki@isp.co.jp>
2024-02-28 11:41:15 +0900

    Fix: changed so that log_config is copied at building
    Signed-off-by: h-suzuki <h-suzuki@isp.co.jp>

--- src/setup.py
+++ src/setup.py
@@ -6,6 +6,7 @@ setup(
     name=package_name,
     version='0.5.0',
     packages=find_packages(exclude=['test']),
+    package_data={package_name: ['*.yaml']},
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
