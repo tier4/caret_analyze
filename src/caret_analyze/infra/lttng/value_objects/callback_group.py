f58dc0c80cdc77a47a3f37efff3b1babd7637013
h-suzuki <h-suzuki@isp.co.jp>
2024-07-01 16:50:21 +0900

    Revert: value_objects/callback_group.py
    Signed-off-by: h-suzuki <h-suzuki@isp.co.jp>

--- src/caret_analyze/infra/lttng/value_objects/callback_group.py
+++ src/caret_analyze/infra/lttng/value_objects/callback_group.py
@@ -26,7 +26,7 @@ class CallbackGroupValueLttng(CallbackGroupValue):
         callback_ids: tuple[str, ...],
         callback_group_id: str,
         callback_group_addr: int,
-        executor_addr: int | None
+        executor_addr: int
     ) -> None:
         super().__init__(
             callback_group_type_name=callback_group_type_name,
@@ -42,7 +42,7 @@ class CallbackGroupValueLttng(CallbackGroupValue):
         return self._callback_group_addr
 
     @property
-    def executor_addr(self) -> int | None:
+    def executor_addr(self) -> int:
         return self._executor_addr
 
 
