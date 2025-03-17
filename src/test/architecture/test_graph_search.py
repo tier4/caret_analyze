89f739ae7066852f729bcb76bfc4d707df1e4229
ISP akm <akm@isp.co.jp>
2025-03-17 19:28:16 +0900

    add test_search_returning_to_start_case
    Signed-off-by: ISP akm <akm@isp.co.jp>

--- src/test/architecture/test_graph_search.py
+++ src/test/architecture/test_graph_search.py
@@ -197,6 +197,20 @@ class TestGraphCore:
 
     #     g.search_paths(0, num-1)
 
+    def test_search_returning_to_start_case(self):
+        g = Graphcore()
+
+        g.add_edge(0, 1)
+        g.add_edge(1, 2)
+        g.add_edge(2, 3)
+        g.add_edge(3, 0)
+
+        r = g.search_paths(0, 0)
+        assert len(r) == 1
+        assert [
+            Graphedgecore(0, 1), Graphedgecore(1, 2), Graphedgecore(2, 3), Graphedgecore(3, 0)
+            ] in r
+
 
 class TestGraph:
 
