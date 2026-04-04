4c018f0fb2de3d05b7bdbeabe11d4edb03f2bf08
ISP akm <akm@isp.co.jp>
2026-04-04 15:29:26 +0900

    chore: suppress mypy [misc] error in Singleton

--- src/caret_analyze/common/singleton.py
+++ src/caret_analyze/common/singleton.py
@@ -28,5 +28,5 @@ class Singleton(object):
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, '_instance'):
-            cls._instance = super(Singleton, cls).__new__(cls)
+            cls._instance = super(Singleton, cls).__new__(cls)  # type: ignore[misc]
         return cls._instance
